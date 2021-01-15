c99f0112c39580b30fb80a01e9457a0b3753313e
lamilekan263 <lamilekan263@gmail.com>
2021-01-15 19:32:27 +0100

    added the offline and online boxes

--- src/Views/Opac.js
+++ src/Views/Opac.js
@@ -2,13 +2,21 @@ import React from 'react'
 
 const Opac = () => {
     return (
-        <div className="container flex  justify-between flex-wrap p-5 mx-auto md:items-center flex-row text-center">
-            <h1 className="text-3xl text-center">
-                This catrgory is divided into two respecively
+      <div className="container   justify-between flex-wrap p-5 mx-auto md:items-center flex-row text-center">
+        <div className="md:flex justify-center mt-10">
+          <div className="h-80 bg-green-700 my-5 md:mr-5 hover:bg-green-500 cursor-pointer rounded-md w-full md:w-96 flex justify-center items-center">
+            <h1 className="text-white md:text-xl">
+              Access our online resources here
             </h1>
-
+          </div>
+          <div className="h-80 bg-green-900 hover:bg-green-500 rounded-md w-full cursor-pointer md:w-96 flex justify-center items-center">
+            <h1 className="text-white md:text-xl">
+              Access our offline resources here
+            </h1>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 export default Opac
